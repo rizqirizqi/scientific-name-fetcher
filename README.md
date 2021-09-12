3c22f282feb38e38b7a6a786281cec18458e009d
Mgs. M. Rizqi Fadhlurrahman <rizqirizqi23@gmail.com>
2021-09-12 23:16:05 +0700

    Add arg options + bugfix (#9)
    * Add arg options
    
    * Update readme

--- README.md
+++ README.md
@@ -1,10 +1,69 @@
 # scientific-name-fetcher
 
+[![contributions welcome][contrib-badge]][contrib-url]
+[![MIT License][license-badge]][license-url]
+
+[![Watch on GitHub][github-watch-badge]][github-watch]
+[![Star on GitHub][github-star-badge]][github-star]
+[![Tweet][twitter-badge]][twitter]
+
+Fetch plants/animals scientific name information from the internet
+
+Current supported sources:
+- Wikipedia
+- Global Biodiversity Information Facility (GBIF)
+
+## Requirements
+- [python >= 3.5](https://www.python.org/downloads/)
+- [pipenv](https://pipenv.pypa.io/en/latest/)
+
 ## How to run
-1. Make sure you have `python3` installed in your machine
-2. Fill the input in `input.txt`, please look at `input.txt.sample`
-3. Run with
+1. Install dependencies
+   ```python
+   cp env.sample .env
+   pipenv --python 3
+   pipenv install
    ```
-   python3 main.py
+2. Fill the input in `input.txt`, please look at `input.txt.sample`
+3. Run
+   ```
+   pipenv run python3 main.py
+   # or
+   pipenv run python3 main.py -i path/to/input-file.txt -o path/to/output-file.txt
    ```
 4. The result will be placed in a file named `result.*.txt`
+
+## Settings
+
+1. Edit .env file
+2. Change the settings
+
+### Available settings:
+
+| Setting                     | Default | Description |
+|-----------------------------|:-------:|------------:|
+| INCLUDE_GBIF_SEARCH         |  True   | Include GBIF search result if it can't find the exact match of the scientific name |
+| AUTO_SEARCH_SIMILAR_SPECIES |  True   | Search GBIF database with similar name from wikipedia result |
+
+## Contributing
+1. Fork this repo
+2. Develop
+3. Create pull request
+4. Tag [@rizqirizqi](https://github.com/rizqirizqi) for review
+5. Merge~~
+
+## License
+
+MIT
+
+[contrib-badge]: https://img.shields.io/badge/contributions-welcome-brightgreen.svg?style=flat-square
+[contrib-url]: https://github.com/rizqirizqi/scientific-name-fetcher/issues
+[license-badge]: https://img.shields.io/npm/l/webpconvert.svg?style=flat-square
+[license-url]: https://github.com/rizqirizqi/webpconvert/blob/master/LICENSE
+
+[github-watch-badge]: https://img.shields.io/github/watchers/rizqirizqi/scientific-name-fetcher.svg?style=social
+[github-watch]: https://github.com/rizqirizqi/scientific-name-fetcher/watchers
+[github-star-badge]: https://img.shields.io/github/stars/rizqirizqi/scientific-name-fetcher.svg?style=social
+[github-star]: https://github.com/rizqirizqi/scientific-name-fetcher/stargazers
+[twitter]: https://twitter.com/intent/tweet?text=Check%20out%20this%20CLI%20converter%20from%20png%2Fjpg%20images%20to%20webp!%20https%3A%2F%2Fgithub.com%2Frizqirizqi%2Fscientific-name-fetcher
+[twitter-badge]: https://img.shields.io/twitter/url/https/github.com/rizqirizqi/scientific-name-fetcher.svg?style=social
--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import os
-import requests
-import json
 import re
-import datetime
+import requests
+import sys, getopt
+from datetime import datetime
 from distutils.util import strtobool
 
 from dotenv import load_dotenv
 
 # Load settings
 load_dotenv()
-WITH_GBIF_SEARCH = strtobool(os.getenv('WITH_GBIF_SEARCH'))
-AUTO_SEARCH_PREDICTED_SPECIES = strtobool(os.getenv('AUTO_SEARCH_PREDICTED_SPECIES'))
+INCLUDE_GBIF_SEARCH = strtobool(os.getenv('INCLUDE_GBIF_SEARCH'))
+AUTO_SEARCH_SIMILAR_SPECIES = strtobool(os.getenv('AUTO_SEARCH_SIMILAR_SPECIES'))
 
 # Functions
 def getDescription(query):
@@ -34,8 +34,11 @@ def getGBIFSearch(query):
   response = requests.get('http://api.gbif.org/v1/species/search?q={}&limit=6'.format(query))
   if response.status_code == 200:
     data = response.json()
-    if data:
-      return data
+    if data and data['count'] > 0:
+      summary = 'GBIF SEARCH:\nResult: {}\n'.format(data['count'])
+      for data in data['results']:
+        summary += '{} {} | {} | {} | Taxonrank: {} > {} > {} > {} > {}\n'.format(data.get('taxonomicStatus'), data.get('rank'), data.get('canonicalName'), data.get('authorship'), data.get('kingdom'), data.get('phylum'), data.get('class'), data.get('order'), data.get('family'))
+      return summary.strip()
     else:
       return 'Not Found'
   else:
@@ -47,46 +50,64 @@ def getGBIFData(query):
     data = response.json()
     if data['matchType'] != 'NONE':
       return 'GBIF MATCH: {} {} | {} {} | {} | {}'.format(data.get('matchType'), data.get('confidence'), data.get('status'), data.get('rank'), data.get('canonicalName'), data.get('authorship'))
-    elif WITH_GBIF_SEARCH:
-      search_result = getGBIFSearch(query)
-      if search_result['count'] > 0:
-        summary = 'GBIF SEARCH:\nResult: {}\n'.format(search_result['count'])
-        for data in search_result['results']:
-          try:
-            summary += '{} {} | {} | {} | Taxonrank: {} > {} > {} > {} > {}\n'.format(data.get('taxonomicStatus'), data.get('rank'), data.get('canonicalName'), data.get('authorship'), data.get('kingdom'), data.get('phylum'), data.get('class'), data.get('order'), data.get('family'))
-          except KeyError:
-            summary += json.dumps(data)
-        return summary
-      else:
-        return 'Not Found'
+    elif INCLUDE_GBIF_SEARCH:
+      return getGBIFSearch(query)
     else:
       return 'Not Found'
   else:
     return 'Error'
 
-# Read Input
-scientific_names = []
-with open('input.txt', 'r') as filehandle:
+def readArgs():
+  inputfile = 'input.txt'
+  outputfile = datetime.now().strftime('result.%Y-%m-%d.%H:%M:%S.txt')
+  usage_hint = 'python main.py -i <inputfile> -o <outputfile>'
+  try:
+    opts, args = getopt.getopt(sys.argv[1:], 'hi:o:', ['ifile=','ofile='])
+  except getopt.GetoptError:
+    print(usage_hint)
+    sys.exit(2)
+  for opt, arg in opts:
+    if opt in ('-h', '--help'):
+        print(usage_hint)
+        sys.exit()
+    elif opt in ('-i', '--ifile'):
+        inputfile = arg
+    elif opt in ('-o', '--ofile'):
+        outputfile = arg
+  print('Input file:', inputfile)
+  print('Output file:', outputfile)
+  return inputfile, outputfile
+
+if __name__ == '__main__':
+  # Setup File
+  inputfile, outputfile = readArgs()
+
+  # Read Input
+  scientific_names = []
+  with open(inputfile, 'r') as filehandle:
     scientific_names = [name.rstrip() for name in filehandle.readlines()]
 
-# Fetch and Write Output
-now = datetime.datetime.now()
-result_filename = now.strftime("result.%Y-%m-%d.%H:%M:%S.txt")
-f = open(result_filename, "a")
-print('Dancing...')
-for name in scientific_names:
-  print(name)
-  description = getDescription(name.split()[0])
-  gbif_query = name
-  if AUTO_SEARCH_PREDICTED_SPECIES and description == 'Not Found':
-    gbif_query = re.search('[A-Z][a-z]+', description)
-  gbif_data = getGBIFData(gbif_query)
-  if gbif_data == 'Not Found' and name.split()[0] != name:
-    gbif_data = getGBIFData(name.split()[0])
-  f.write(name)
-  f.write('\n')
-  f.write(description or 'Not Found')
-  f.write('\n')
-  f.write(gbif_data or 'Not Found')
-  f.write('\n')
-  f.write('\n')
+  # Fetch and Write Output
+  f = open(outputfile, 'a')
+  print('Starting, it may take a while, please wait...')
+  for name in scientific_names:
+    print('Looking up:', name)
+    # Get Description
+    description = getDescription(name)
+    # Get GBIF Data from name
+    gbif_data = getGBIFData(name)
+    # Get GBIF Data from similar name
+    if gbif_data == 'Not Found' and AUTO_SEARCH_SIMILAR_SPECIES and description != 'Not Found':
+      similar_name = re.search(name.split()[0] + ' .+', description)
+      gbif_data = getGBIFData(similar_name)
+    # Get GBIF Data from first word
+    if gbif_data == 'Not Found' and name.split()[0] != name:
+      gbif_data = getGBIFData(name.split()[0])
+    f.write('### ' + name)
+    f.write('\n')
+    f.write(description or 'Not Found')
+    f.write('\n')
+    f.write(gbif_data or 'Not Found')
+    f.write('\n')
+    f.write('\n')
+  print('Done! :D')
