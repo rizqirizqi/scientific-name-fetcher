43401425eba32502f46c2cfbf420065fd6e8a117
Jonas Ferreira <jonasfscc@gmail.com>
2021-10-31 22:53:43 -0300

    Fix formatting

--- tests.py
+++ tests.py
@@ -4,12 +4,14 @@ from unittest.mock import patch
 import main
 from datetime import date
 
+
 class Tests(TestCase):
 
     def test_args(self):
-        testargs = ["main", "-i", "my_test_input.txt", "-o", "my_test_output.txt"]
+        testargs = ["main", "-i", "my_test_input.txt",
+                    "-o", "my_test_output.txt"]
         with patch.object(sys, 'argv', testargs):
-            inputfile, outputfile = main.readArgs() 
+            inputfile, outputfile = main.readArgs()
             self.assertEqual(inputfile, "my_test_input.txt")
             self.assertEqual(outputfile, "my_test_output.txt")
 
@@ -18,10 +20,12 @@ class Tests(TestCase):
         with patch.object(sys, 'argv', testargs):
             with patch('main.datetime') as mock_datetime:
                 mock_datetime.now.return_value = date(2021, 10, 31)
-                mock_datetime.side_effect = lambda *args, **kw: date(*args, **kw)
-                inputfile, outputfile = main.readArgs() 
+                mock_datetime.side_effect = lambda *args, **kw: date(
+                    *args, **kw)
+                inputfile, outputfile = main.readArgs()
                 self.assertEqual(inputfile, "input.txt")
                 self.assertEqual(outputfile, "result.2021-10-31.00:00:00.txt")
+
 
 if __name__ == '__main__':
     test_main()
